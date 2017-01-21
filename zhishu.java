fc44d7349573a5a754fa12258f261f309df36860
kobyhuazai <aierqiaoshuya@gmail.com>
2017-01-21 09:31:59 +0800

    add zhishu

---
+++ zhishu.java
@@ -0,0 +1,35 @@
+public  class zhishu{
+     public static void main(String[] args){
+           int count=0;
+           for(int i=101;i<=200;i++){
+                 for(int j=2;j<=i;j++){
+                      if(j==i){
+                         System.out.println(j);
+                         count++;
+                     
+                       }
+                       else if(i%j == 0){
+                       break;
+
+
+                       }                  
+ 
+
+                      
+                 }               
+
+            
+       
+           }
+           System.out.println("We have " + count +" zhishu");
+
+     
+
+    
+  
+      }
+
+
+
+
+}
