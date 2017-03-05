d702e39426dc63ae578a11becb53945efdb4eb21
kobyhuazai <aierqiaoshuya@gmail.com>
2017-03-05 19:55:31 +0800

    the last

--- Snake Game/Settings.java
+++
@@ -1,9 +0,0 @@
-
-public class Settings {
-	public static final int DEFAULT_NODE_SIZE = 5;
-	public static final int DEFAULT_GRID_WIDTH = 1500;
-	public static final int DEFAULT_GRID_HEIGHT = 1500;
-	
-	public static final long DEFAULT_MOVE_INTERVAL = 200;
-	
-}
